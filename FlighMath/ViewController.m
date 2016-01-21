1a6c14d188a1c9b7a774a4e18f9a8675b06b712a
AdamJoniecPride <adam.joniec@o2.pl>
2016-01-21 11:24:38 +0100

    merged

--- FlighMath/ViewController.m
+++ FlighMath/ViewController.m
@@ -19,27 +19,7 @@
     [super viewDidLoad];
     
     NSLog(@"%ld" , [self factorial:5]);
-    NSLog(@"%lf", [self sqrt:9]);
-    
-}
-
-- (NSInteger)factorial:(NSInteger)a {
-    
-    if (a < 0) {
-        NSLog(@"Error");
-        return -1;
-    }
-    NSInteger b = 1;
-    for (NSInteger i = 2 ; i <= a; i++) {
-        b*=i;
-    }
-    return b;
-    
-}
-
-- (CGFloat) sqrt:(CGFloat)c {
-    
-    return sqrt(c);
+    NSLog(@"%@", [self sqrt:@2.25]);
     
 }
 
